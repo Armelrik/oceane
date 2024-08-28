dd1537cf674f35c7e3ae763904bcfbfd5d843e23
armelrik <armelrik@hotmail.com>
2024-08-28 18:10:30 +0000

    cooler

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import { Agdasima } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
@@ -8,13 +9,23 @@ const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
   variable: '--font-poppins',
+  display: 'swap', 
+  adjustFontFallback: false,
+})
+
+const agdasima = Agdasima({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-agdasima',
+  display: 'swap', 
+  adjustFontFallback: false,
 })
 
 export const metadata: Metadata = {
   title: 'Oceane',
   description: 'Oceane is a platform for event management.',
   icons: {
-    icon: '/assets/images/logo.svg'
+    icon: '/assets/images/logo-color.svg'
   }
 }
 
@@ -25,8 +36,8 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={poppins.variable}>{children}</body>
+      <html lang="en" className={`${agdasima.variable}`}>
+        <body className={`${agdasima.variable}`}>{children}</body>
       </html>
     </ClerkProvider>
   )
