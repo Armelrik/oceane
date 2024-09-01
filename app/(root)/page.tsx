96219b0d51adfd2143fcfbea6af920fbfb113916
Armelrik <armelrik@hotmail.com>
2024-09-01 10:11:45 +0000

    commit_10

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,14 +3,21 @@ import Collection from "@/components/shared/Collection";
 import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home() {
-  const events = await getAllEvents({
-    query: '', category:'', page:1, limit: 6 
-  })
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
 
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
 
   return (
   <>
@@ -47,8 +54,8 @@ export default async function Home() {
         emptyStateSubtext="Revenez plus tard"
         collectionType="All_Events"
         limit={6}
-        page={1}
-        totalPages={2}
+        page={page}
+        totalPages={events?.totalPages}
       />
     </section>
   </>
