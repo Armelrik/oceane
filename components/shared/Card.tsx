7389a1234832ec548629a5f0dbf4706911e7acf4
Armelrik <armelrik@hotmail.com>
2024-09-04 20:48:38 +0000

    commit_14

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -44,6 +44,8 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
             {event.category.name}
           </p>
+          <span className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
+          {event.location}</span>
         </div>}
 
         <p className="p-medium-16 p-medium-18 text-grey-500">
